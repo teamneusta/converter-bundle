13e4570112a60922b100c1c0d1c4dd2cf2e9b8b9
Michael Albrecht <m.albrecht@neusta.de>
2023-01-14 23:05:33 +0100

    Update Address.php
    remove unnecessary phpdocs here

--- tests/Fixtures/Model/Address.php
+++ tests/Fixtures/Model/Address.php
@@ -11,72 +11,44 @@ class Address
     private string $street;
     private string $streetNo;
 
-    /**
-     * @return string
-     */
     public function getPostalCode(): string
     {
         return $this->postalCode;
     }
 
-    /**
-     * @param string $postalCode
-     * @return Address
-     */
     public function setPostalCode(string $postalCode): Address
     {
         $this->postalCode = $postalCode;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getCity(): string
     {
         return $this->city;
     }
 
-    /**
-     * @param string $city
-     * @return Address
-     */
     public function setCity(string $city): Address
     {
         $this->city = $city;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getStreet(): string
     {
         return $this->street;
     }
 
-    /**
-     * @param string $street
-     * @return Address
-     */
     public function setStreet(string $street): Address
     {
         $this->street = $street;
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getStreetNo(): string
     {
         return $this->streetNo;
     }
 
-    /**
-     * @param string $streetNo
-     * @return Address
-     */
     public function setStreetNo(string $streetNo): Address
     {
         $this->streetNo = $streetNo;
