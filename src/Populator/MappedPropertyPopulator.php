7a69d590a3e25753db5584f4c3c2487489c7ae3b
Jacob Dreesen <jacob@hdreesen.de>
2023-01-19 00:34:31 +0100

    Allow MappedPropertyPopulator to actually map values

--- src/Populator/MappedPropertyPopulator.php
+++ src/Populator/MappedPropertyPopulator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Neusta\ConverterBundle\Populator;
 
+use Neusta\ConverterBundle\Exception\PopulationException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
@@ -16,19 +17,32 @@ use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 final class MappedPropertyPopulator implements Populator
 {
     private PropertyAccessorInterface $accessor;
+    private \Closure $mapper;
 
+    /**
+     * @param \Closure(mixed, C):mixed|null $mapper
+     */
     public function __construct(
         private string $targetProperty,
         private string $sourceProperty,
+        ?\Closure $mapper = null,
         PropertyAccessorInterface $accessor = null,
     ) {
+        $this->mapper = $mapper ?? static fn ($v) => $v;
         $this->accessor = $accessor ?? PropertyAccess::createPropertyAccessor();
     }
 
+    /**
+     * @throws PopulationException
+     */
     public function populate(object $target, object $source, ?object $ctx = null): void
     {
-        $this->accessor->setValue($target, $this->targetProperty,
-            $this->accessor->getValue($source, $this->sourceProperty),
-        );
+        try {
+            $this->accessor->setValue($target, $this->targetProperty,
+                ($this->mapper)($this->accessor->getValue($source, $this->sourceProperty), $ctx),
+            );
+        } catch (\Throwable $exception) {
+            throw new PopulationException($this->sourceProperty, $this->targetProperty, $exception);
+        }
     }
 }
