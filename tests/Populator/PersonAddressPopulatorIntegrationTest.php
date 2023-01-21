715a28fd6a262061669093aa1a38e55eefb28d76
Jacob Dreesen <jacob@hdreesen.de>
2023-01-21 14:10:17 +0100

    Clean up the test-suite

--- tests/Populator/PersonAddressPopulatorIntegrationTest.php
+++ tests/Populator/PersonAddressPopulatorIntegrationTest.php
@@ -7,27 +7,19 @@ namespace Neusta\ConverterBundle\Tests\Populator;
 use Neusta\ConverterBundle\Converter\DefaultConverterContext;
 use Neusta\ConverterBundle\Exception\PopulationException;
 use Neusta\ConverterBundle\Populator\Populator;
-use Neusta\ConverterBundle\Tests\BundleKernelTestCase;
 use Neusta\ConverterBundle\Tests\Fixtures\Model\Address;
 use Neusta\ConverterBundle\Tests\Fixtures\Model\Person;
 use Neusta\ConverterBundle\Tests\Fixtures\Model\PersonAddress;
 use Neusta\ConverterBundle\Tests\Fixtures\Model\UnknownType;
 use Neusta\ConverterBundle\Tests\Fixtures\Model\User;
-use Neusta\ConverterBundle\Tests\Fixtures\Populator\PersonAddressPopulator;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class PersonAddressPopulatorIntegrationTest extends BundleKernelTestCase
+class PersonAddressPopulatorIntegrationTest extends KernelTestCase
 {
-    /** @var Populator<User, Person, DefaultConverterContext> $populator */
-    private Populator $populator;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
     public function testPopulate_regular_case(): void
     {
-        $this->populator = $this->getContainer()->get('test.person.address.populator');
+        /** @var Populator<User, Person, DefaultConverterContext> $populator */
+        $populator = self::getContainer()->get('test.person.address.populator');
         $address = (new Address())
             ->setCity('Bremen')
             ->setPostalCode('28217')
@@ -38,13 +30,15 @@ class PersonAddressPopulatorIntegrationTest extends BundleKernelTestCase
 
         $person = (new Person())->setAddress(new PersonAddress());
 
-        $this->populator->populate($person, $user);
+        $populator->populate($person, $user);
 
         self::assertEquals('24', $person->getAddress()->getStreetNo());
     }
+
     public function testPopulate_wrong_source_type(): void
     {
-        $this->populator = $this->getContainer()->get('test.person.wrong.source.type.populator');
+        /** @var Populator<User, Person, DefaultConverterContext> $populator */
+        $populator = self::getContainer()->get('test.person.wrong.source.type.populator');
 
         $user = (new User())->setFieldWithUnknownType(new UnknownType());
 
@@ -52,13 +46,15 @@ class PersonAddressPopulatorIntegrationTest extends BundleKernelTestCase
 
         $this->expectException(PopulationException::class);
         $this->expectExceptionMessageMatches("/Population Exception \(fieldWithUnknownType -> address\): (.*)/");
-        $this->populator->populate($person, $user);
+        $populator->populate($person, $user);
 
         self::assertEquals('24', $person->getAddress()->getStreetNo());
     }
+
     public function testPopulate_wrong_converter(): void
     {
-        $this->populator = $this->getContainer()->get('test.person.wrong.converter.populator');
+        /** @var Populator<User, Person, DefaultConverterContext> $populator */
+        $populator = self::getContainer()->get('test.person.wrong.converter.populator');
         $address = (new Address())
             ->setCity('Bremen')
             ->setPostalCode('28217')
@@ -71,7 +67,7 @@ class PersonAddressPopulatorIntegrationTest extends BundleKernelTestCase
 
         $this->expectException(PopulationException::class);
         $this->expectExceptionMessageMatches("/Population Exception \(address -> address\): (.*)/");
-        $this->populator->populate($person, $user);
+        $populator->populate($person, $user);
 
         self::assertEquals('24', $person->getAddress()->getStreetNo());
     }
