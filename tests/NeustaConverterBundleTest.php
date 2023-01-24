fa4a45fb2f53cfc96e3bbfe48af19ef4e024a7e0
Michael Albrecht <m.albrecht@neusta.de>
2023-01-24 12:51:48 +0100

    Add test for NeustaConverterBundle

---
+++ tests/NeustaConverterBundleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neusta\ConverterBundle\Tests;
+
+use Neusta\ConverterBundle\NeustaConverterBundle;
+use PHPUnit\Framework\TestCase;
+
+class NeustaConverterBundleTest extends TestCase
+{
+    public function testThatBundlePathIsCurrentDir(): void
+    {
+        self::assertStringEndsWith('src', (new NeustaConverterBundle)->getPath());
+    }
+}
