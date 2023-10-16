e176a10aa3fbabf7895d47c9705383cd92b99e37
Michael Albrecht <m.albrecht@neusta.de>
2023-10-16 09:21:01 +0200

    Update tests/Populator/PropertyMappingPopulatorTest.php
    Co-authored-by: Jacob Dreesen <j.dreesen@neusta.de>

--- tests/Populator/PropertyMappingPopulatorTest.php
+++ tests/Populator/PropertyMappingPopulatorTest.php
@@ -39,6 +39,6 @@ class PropertyMappingPopulatorTest extends TestCase
 
         $populator->populate($person, $address);
 
-        self::assertEquals('Street', $person->getAddress()->getStreet());
+        self::assertSame($address, $person->getAddress());
     }
 }
