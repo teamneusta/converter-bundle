5859447fae2d1fec81ec9b78e0327e45f014c6a9
Michael Albrecht (personal) <m.albrecht@neusta.de>
2025-04-01 18:45:52 +0200

    [Overview] initial commit

---
+++ src/Command/ShowConvertersPopulatorsCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neusta\ConverterBundle\Command;
+
+use Neusta\ConverterBundle\Dump\InspectedServicesRegistry;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Twig\Environment as TwigEnvironment;
+
+#[AsCommand(name: 'neusta_converter:show', description: 'show converters, populators and factories')]
+class ShowConvertersPopulatorsCommand extends Command
+{
+    public function __construct(
+        private readonly InspectedServicesRegistry $registry,
+        private readonly TwigEnvironment $twig,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption('html', null, InputOption::VALUE_REQUIRED, 'Path to the HTML file for static output')
+            ->setHelp(<<<'HELP'
+                This command displays a structured list of all tagged services
+                that act as converters, factories or populators – including their constructor arguments.
+
+                Optionally, you can generate a static HTML documentation using the --html option:
+
+                bin/console neusta_converter:show --html=var/converter.html
+
+                The HTML file contains a linkable overview and can be used, for example,
+                as part of developer documentation.
+                HELP
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $htmlPath = $input->getOption('html');
+
+        if ($htmlPath) {
+            $html = $this->twig->render('@NeustaConverter/debug/service_inspector.html.twig', [
+                'services' => array_merge($this->registry->allConverters(), $this->registry->allPopulators(), $this->registry->allFactories()),
+            ]);
+
+            file_put_contents($htmlPath, $html);
+            $output->writeln("✅ HTML-Datei gespeichert: $htmlPath");
+
+            return Command::SUCCESS;
+        }
+
+        $output->writeln('<info>🎯 Converter:</info>');
+        $this->describeServices($this->registry->allConverters(), $output);
+
+        $output->writeln('');
+        $output->writeln('<info>🎯 Factories:</info>');
+        $this->describeServices($this->registry->allFactories(), $output);
+
+        $output->writeln('');
+        $output->writeln('<info>🎯 Populatoren:</info>');
+        $this->describeServices($this->registry->allPopulators(), $output);
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param iterable<string, array<string|int, string|array<mixed>>> $services
+     */
+    private function describeServices(iterable $services, OutputInterface $output): void
+    {
+        foreach ($services as $id => $service) {
+            $class = $service['class'];
+            if (\is_string($class)) {
+                $output->writeln("🔧 <info>$id</info>: <comment>$class</comment>");
+            }
+
+            $this->writeArgumentsArray($service['arguments'], $output, 1); // @phpstan-ignore-line
+            $output->writeln('');
+        }
+    }
+
+    /**
+     * @param array<string|int, string|array<mixed>> $arguments
+     */
+    private function writeArgumentsArray(array $arguments, OutputInterface $output, int $level): void
+    {
+        foreach ($arguments as $key => $argument) {
+            if (\is_array($argument)) {
+                $output->writeln(str_repeat('  ', $level) . "- <info>$key</info>:");
+                $this->writeArgumentsArray($argument, $output, $level + 1);
+            } else {
+                $output->writeln(str_repeat('  ', $level) . "- <info>$key</info>: <comment>$argument</comment>");
+            }
+        }
+    }
+}
--- src/NeustaConverterBundle.php
+++ src/NeustaConverterBundle.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Neusta\ConverterBundle;
 
+use Neusta\ConverterBundle\DependencyInjection\Compiler\ServiceInspectorPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NeustaConverterBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new ServiceInspectorPass());
+    }
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
