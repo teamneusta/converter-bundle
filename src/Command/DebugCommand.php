6b20d8abc1a6602639d96e7098dfbea166d40936
Sebastian Bock <11462912+sebo32@users.noreply.github.com>
2025-12-12 14:09:41 +0100

    Update DebugCommand.php

--- src/Command/DebugCommand.php
+++ src/Command/DebugCommand.php
@@ -14,7 +14,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Translation\Translator;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment as TwigEnvironment;
 
