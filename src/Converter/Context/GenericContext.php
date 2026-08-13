a2b01fdb744bbdafb5996d14eedd1606045b49b7
Jacob Dreesen <j.dreesen@neusta.de>
2026-08-13 16:24:48 +0200

    Move `GenericContext` deprecation trigger to constructor

--- src/Converter/Context/GenericContext.php
+++ src/Converter/Context/GenericContext.php
@@ -6,18 +6,21 @@ namespace Neusta\ConverterBundle\Converter\Context;
 
 use Neusta\ConverterBundle\Context;
 
-trigger_deprecation(
-    'teamneusta/converter-bundle',
-    '1.10.0',
-    '"%s" is deprecated, use "%s" instead.',
-    GenericContext::class,
-    Context::class,
-);
-
 class GenericContext
 {
     /** @var array<string, mixed> */
     protected array $values = [];
+
+    public function __construct()
+    {
+        trigger_deprecation(
+            'teamneusta/converter-bundle',
+            '1.10.0',
+            '"%s" is deprecated, use "%s" instead.',
+            self::class,
+            Context::class,
+        );
+    }
 
     public function hasKey(string $key): bool
     {
